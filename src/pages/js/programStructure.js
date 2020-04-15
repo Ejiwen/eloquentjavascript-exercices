3486169dc35b749f454a4173a88fab68cf0caf69
Cheikhany ould Ejiwen <skibnewrim@gmail.com>
2020-04-14 17:30:06 -0700

    add solution for FizzBuzz

--- src/pages/js/programStructure.js
+++ src/pages/js/programStructure.js
@@ -3,3 +3,11 @@ let str = "#";
 for (let i = 0; i < 7; i++, str += "#") {
   console.log(str);
 }
+
+/* Exo 2 */
+for (let i = 0; i <= 100; i++) {
+  let str = "";
+  if (i % 3 === 0) str += "Fizz";
+  if (i % 5 === 0) str += "Buzz";
+  console.log(str || i);
+}
