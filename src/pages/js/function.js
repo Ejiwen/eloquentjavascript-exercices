bfb53204505b1c353fda340ee4839d57697d37da
Cheikhany ould Ejiwen <skibnewrim@gmail.com>
2020-04-16 16:35:51 -0700

    function - add exo 2 solution - recursion

--- src/pages/js/function.js
+++ src/pages/js/function.js
@@ -3,3 +3,18 @@ min = (x, y) => {
   if (x < y) return x;
   else return y;
 };
+
+/* Ex0 2 : Recursion */
+const isEven = n => {
+  if (n < 0) return "numbre is negative";
+  if (n == 0) return true;
+  if (n == 1) return false;
+  if (n > 1) return isEven(n - 2);
+};
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-2));
+// → numbre is negative
