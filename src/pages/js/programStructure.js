f4826428603f7fac189d3788bca33f2038288395
Cheikhany ould Ejiwen <skibnewrim@gmail.com>
2020-04-15 17:48:42 -0700

    Add exo 3 Chessboard - solution

--- src/pages/js/programStructure.js
+++ src/pages/js/programStructure.js
@@ -11,3 +11,14 @@ for (let i = 0; i <= 100; i++) {
   if (i % 5 === 0) str += "Buzz";
   console.log(str || i);
 }
+
+/* Exo 3: Chessboard */
+let str = "\n";
+for (let i = 0; i < 8; i++, str += "\n") {
+  if (i % 2 === 0) str += " ";
+  for (let j = 0; j < 8; j++) {
+    if (j % 2 != 0) str += "#";
+    else str += " ";
+  }
+}
+console.log(str);
