d9c99e68ac2f41dabcfb36dbf9844c85c25c32b6
Cheikhany ould Ejiwen <skibnewrim@gmail.com>
2020-04-18 20:47:40 -0700

    add new function

--- src/pages/js/dataStructure.js
+++ src/pages/js/dataStructure.js
@@ -12,3 +12,13 @@ const sum = range => {
 };
 
 console.log(sum(range(1, 10)));
+
+const range = (start, end, step = 1) => {
+  let rangeTab = [];
+  for (let i = start; step < 0 ? i >= end : i <= end; i = step + i)
+    rangeTab.push(i);
+  return rangeTab;
+};
+
+console.log(range(5, 2, -1));
+// → [5, 4, 3, 2]
