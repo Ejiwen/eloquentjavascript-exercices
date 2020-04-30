a5b07ee1e5ff3a3ec9e79513334eaf8e69b2f547
Cheikhany ould Ejiwen <skibnewrim@gmail.com>
2020-04-29 21:09:11 -0700

    add deepEqual function
    Deep comparison between two variables .

--- src/pages/js/dataStructure.js
+++ src/pages/js/dataStructure.js
@@ -129,3 +129,31 @@ function nth(list,position){
 
 console.log(nth(arrayToList([10, 20, 30]), 2));
 // → 20
+/********* Implemente a list via an array *********/
+
+/********* Deep comparison *********/
+
+function deepEqual(a, b) {
+  if (a === b) return true;
+  
+  if (a == null || typeof a != "object" ||
+      b == null || typeof b != "object") return false;
+
+  let keysA = Object.keys(a), keysB = Object.keys(b);
+
+  if (keysA.length != keysB.length) return false;
+
+  for (let key of keysA) {
+    if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+  }
+
+  return true;
+}
+
+let obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
