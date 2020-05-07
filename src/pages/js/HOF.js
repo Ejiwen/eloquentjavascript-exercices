074554b64ef8edd8e8bf8b6228a4b0d3dfbd9c77
Cheikhany ould Ejiwen <skibnewrim@gmail.com>
2020-05-06 19:01:30 -0700

    Add my solution for reduce method
    For EXO 1 : Flattening

---
+++ src/pages/js/HOF.js
@@ -0,0 +1,15 @@
+/********** EXO 1 : Flattening **********/
+/***** My Solution *****/
+const concatArr = (arr, combine) => {
+  let current = [];
+  arr.forEach(l => {
+    current = combine(current, l);
+  });
+  return current;
+};
+
+let arrays = [[1, 2, 3], [4, 5], [6]];
+console.log(concatArr(arrays, (a, b) => a.concat(b)));
+// → [1, 2, 3, 4, 5, 6]
+
+/**********  **********/
