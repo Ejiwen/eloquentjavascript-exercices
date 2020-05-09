d60d06a6de02d8bfbd109c41f800a37149361b62
Cheikhany ould Ejiwen <skibnewrim@gmail.com>
2020-05-09 11:33:25 -0700

    add Everything solution (using For loop)

--- src/pages/js/HOF.js
+++ src/pages/js/HOF.js
@@ -32,3 +32,18 @@ loop(3, n => n > 0, n => n - 1, console.log);
 // → 3
 // → 2
 // → 1
+
+/**** Everything *******/
+function every(array, test) {
+  for (let i = 0; i < array.length; i++) {
+    if (!test(array[i])) return false;
+  }
+  return true;
+}
+
+console.log(every([1, 3, 5], n => n < 10));
+// → true
+console.log(every([2, 1, 16], n => n < 10));
+// → false
+console.log(every([], n => n < 10));
+// → true
