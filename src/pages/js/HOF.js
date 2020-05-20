1f3a8f2d38dbf223edcd66f4e54ff5c4a65d1aa5
Cheikhany ould Ejiwen <skibnewrim@gmail.com>
2020-05-20 16:01:35 -0700

    add the solution for Dominant writing direction

--- src/pages/js/HOF.js
+++ src/pages/js/HOF.js
@@ -51,3 +51,20 @@ console.log(every([2, 1, 16], n => n < 10));
 // → false
 console.log(every([], n => n < 10));
 // → true
+
+/**************** Dominant writing direction (NOT ME)****************/
+function dominantDirection(text) {
+  let counted = countBy(text, char => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.direction : "none";
+  }).filter(({ name }) => name != "none");
+
+  if (counted.length == 0) return "ltr";
+
+  return counted.reduce((a, b) => (a.count > b.count ? a : b)).name;
+}
+
+console.log(dominantDirection("Hello!"));
+// → ltr
+console.log(dominantDirection("Hey, مساء الخير"));
+// → rtl
